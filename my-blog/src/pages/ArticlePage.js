20802f267fb909a8616e3c75bab025937cf2fff3
Moyen Uddin <moyen.ruet@yahoo.com>
2024-05-13 11:59:59 +0200

    display individual article

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { parsePath, useParams } from "react-router-dom";
 import NavBar from "../NavBar";
 import articles from "./article-content";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
   return (
     <div>
       <NavBar />
-      <p></p>
-      <h1>This is the article for the article: {articleId}</h1>
+      <div>
+        <p></p>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   );
 };
