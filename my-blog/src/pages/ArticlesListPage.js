b435bc68aade86d665552c700a0684e6d3ab19e5
Moyen Uddin <moyen.ruet@yahoo.com>
2024-05-13 14:39:57 +0200

    show all articles as list

--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,12 +1,26 @@
 import React from "react";
 import NavBar from "../NavBar";
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticlesListPage = () => {
   return (
     <div>
       <NavBar />
       <p></p>
-      <h2>THis is the Articles List page!</h2>
+      <h1>All Articles</h1>
+      {articles.map((article, i) => (
+        <>
+          <Link
+            key={article.name}
+            className="article-list-item"
+            to={`/articles/${article.name}`}
+          >
+            <h3>{article.name}</h3>
+          </Link>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </>
+      ))}
     </div>
   );
 };
